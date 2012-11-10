b1bbfa6c2ff0b7e3e1ae6df06b1b9b72f30bce26
Luke Chadwick <luke.a.chadwick@gmail.com>
2012-11-10 12:41:12 +1100

    Make mongoose connect and work

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
+var mongoose = require('mongoose');
+var mongoUri = 'mongodb://user:password@host:27017/database';
+// var db = mongoose.createConnection(mongoUri);
+// 
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+// 
+// });
+mongoose.connect(mongoUri, function(err) {
+  if (err) throw err;
+  console.log('Connected')
+});
+
+var newsEntrySchema = new mongoose.Schema({
+    title: String,
+    topic: String,
+    weighting: Number,
+    loc : {lat: Number,  lng: Number },
+});
+var NewsEntry = mongoose.model('NewsEntry', newsEntrySchema);
+
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  
+  NewsEntry.find({}).limit(1).execFind(function (err,data) {
+    res.end(JSON.stringify(data));
+  });
 }).listen(8000);
 
 console.log('Server running at http://0.0.0.0:8000/');

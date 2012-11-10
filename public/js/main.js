e009f9f4d259e9eb9b5cc2191373ecc5a5dd145b
Eric Entzel <eric@ubermac.net>
2012-11-10 11:31:32 +1100

    big map

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,10 @@
+/* global google $ */
 
+$(document).ready(function () {
+    var mapOpts = {
+        center: new google.maps.LatLng(-27.606286439866967, 135.94821875),
+        zoom: 4,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOpts);
+});

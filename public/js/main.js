cdd2449731dc144f18aae454d09ca3f9a4d736ab
Eric Entzel <eric@ubermac.net>
2012-11-10 13:02:04 +1100

    Use geolocation when available

--- public/js/main.js
+++ public/js/main.js
@@ -4,20 +4,31 @@ var locations = [{"post-code":"3000","street-address":"318 Russell Street","subu
 
 $(document).ready(function () {
     var mapOpts = {
-        center: new google.maps.LatLng(-27.606286439866967, 135.94821875),
+        center: new google.maps.LatLng(38.9695, -98.1347),
         zoom: 4,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var theMap = new google.maps.Map(document.getElementById('map'), mapOpts);
 
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(p) {
+            mapOpts.center = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
+            mapOpts.zoom = 11;
+            initMap(mapOpts);
+        }, initMap.bind(this, mapOpts), {timeout: 3000});
+    }
+    else {
+        initMap(mapOpts);
+    }
+
+});
+
+function initMap(mapOpts) {
+    var theMap = new google.maps.Map(document.getElementById('map'), mapOpts);
     var theMarkers = locations.map(function (l) {
         return new google.maps.Marker({
             position: new google.maps.LatLng(l.location.latitude, l.location.longitude),
             title: l.full_address
         });
     });
-
     var theClusterer = new MarkerClusterer(theMap, theMarkers);
-
-
-});
+}

469d80fe3c7f136504aa6ef3a7e98f31e1ac016b
Eric Entzel <eric@ubermac.net>
2012-11-10 15:34:45 +1100

    Unfortunately bottom right map type controls aren't smart enough to avoid dropping down outside the viewport.

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var mapOpts = {
         center: new google.maps.LatLng(38.9695, -98.1347),
         zoom: 4,
-        mapTypeControlOptions: { position: google.maps.ControlPosition.BOTTOM_LEFT },
+        mapTypeControlOptions: { position: google.maps.ControlPosition.TOP_LEFT },
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 

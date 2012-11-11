dbb424f3840eb3b1cbb96d56ee6713d9b82e0edd
Eric Entzel <eric@ubermac.net>
2012-11-11 19:43:49 +1100

    JS formatting options for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((js2-mode
+  (indent-tabs-mode . nil)
+  (fill-column . 120)
+  (js2-basic-offset . 2)))
